64eb4d8641c3a7e0e314bcb0e50917628a7cd692
yotamHak <yotam.hakim@gmail.com>
2016-10-04 10:38:27 +0300

    Updated
    Added better Steam API Check
    Added link store URL on update
    Better usage of variables and some other Fixes

--- GAS_Code.js
+++ GAS_Code.js
@@ -1,29 +1,52 @@
 // Variables
+
+// Steam API
+var STEAM_API_KEY = "00D5B3E37E04C5E734BF1B98A3CA9ADE";
+var STEAM_ID64 = "76561197967370369";
+
+// Headers
 var IS_THERE_ANY_DEAL_HEADER = "isthereanydeal URL";
 var TIMESTAMP_HEADER = "Added";
 var ON_UPDATE_HEADER = "Name";
-var SHEET_NAME = 'Keys';
-var BUNDLED_BACKGROUND_COLOR = '#f6b26b';
-var NON_BUNDLED_BACKGROUND_COLOR = '#66ccff';
-var STEAM_API_KEY = ""; // Must fill
-var STEAM_ID64 = ""; // Must fill
 var OWN_STATUS_HEADER = "Own Status";
+var STEAM_URL_HEADER = "Steam URL";
+
+// Texts
 var MISSING_TEXT = "Missing";
 var OWN_TEXT = "Own";
+
+// Colors
+var BUNDLED_BACKGROUND_COLOR = '#f6b26b';
+var NON_BUNDLED_BACKGROUND_COLOR = '#66ccff';
+
+var SHEET_NAME = 'Keys';
+
 //-------------------------------------------------------------------------------------------
+
+var ss, title, headers, changedCell;
 
 function onEdit(event)
 {
-  Logger.log(SpreadsheetApp.getActive().getUrl());
+  ss = event.source.getSheetByName(SHEET_NAME);
+  title = _encodeName(event.value);
+  headers = ss.getRange(1, 1, 1, ss.getLastColumn()).getValues();
+  changedCell= event.source.getActiveRange().getA1Notation();
+  
   addDateStamp(event);
   handleBundled(event); 
 }
 
-function addIsThereAnyDealUrl(event){
-  var ss = event.source.getSheetByName(SHEET_NAME);
-  var title = _encodeName(event.value);
+// Adds an Steam url---------------------------------------------------------------------
+function addSteamUrl(event, appId){
+  var url = "http://store.steampowered.com/app/" + appId + "/";
+  var urlCol = headers[0].indexOf(STEAM_URL_HEADER);
   
-  var headers = ss.getRange(1, 1, 1, ss.getLastColumn()).getValues();
+  var urlToUpdate = ss.getRange(event.source.getActiveRange().getRow(), urlCol + 1);
+  urlToUpdate.setValue(url);
+}
+
+// Adds an isThereAnyDeal url---------------------------------------------------------------------
+function addIsThereAnyDealUrl(event){
   var isthereanydealUrl = "https://isthereanydeal.com/#/page:game/info?plain=" + title;
   var urlCol = headers[0].indexOf(IS_THERE_ANY_DEAL_HEADER);
   
@@ -31,28 +54,26 @@ function addIsThereAnyDealUrl(event){
   urlToUpdate.setValue(isthereanydealUrl);
 }
 
+// Checks to see if you own the game or not, needs STEAM_API_KEY and STEAM_ID64--------------------
 function getSteamOwnStatusAndSetIt(text, event){
-  var myString = "something format_abc";
   var myRegexp = /([steampowered.com]*)(\d+)/g;
   var match = myRegexp.exec(text);
   var appID = match[0];
+  
+  addSteamUrl(event, appID);
   
   var data = {
     "steamid":STEAM_ID64,
     "appids_filter": [parseInt(appID)]
   };
   
-  //var url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=" + STEAM_API_KEY + "&input_json=" + JSON.stringify(data);
-  var url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=" + STEAM_API_KEY + "&steamid=" + STEAM_ID64;
-
+  var payload = "&input_json=" + encodeURIComponent(JSON.stringify(data));
+  var url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=" + STEAM_API_KEY + payload;
   var response = UrlFetchApp.fetch(url);
-  //Logger.log(response);
   var text = response.getContentText();
   
-      var ss = event.source.getSheetByName(SHEET_NAME);
-    var headers = ss.getRange(1, 1, 1, ss.getLastColumn()).getValues();
-    var urlCol = headers[0].indexOf(OWN_STATUS_HEADER);
-    var ownStatusToUpdate = ss.getRange(event.source.getActiveRange().getRow(), urlCol + 1);
+  var urlCol = headers[0].indexOf(OWN_STATUS_HEADER);
+  var ownStatusToUpdate = ss.getRange(event.source.getActiveRange().getRow(), urlCol + 1);
   
   if (text.indexOf(appID) !== -1){
     ownStatusToUpdate.setValue(OWN_TEXT);
@@ -62,12 +83,9 @@ function getSteamOwnStatusAndSetIt(text, event){
   }
 }
 
+// Checks if the game have been bundled before and sets the right color--------------------------------
 function handleBundled(event){
   addIsThereAnyDealUrl(event);
-  
-  var ss = event.source.getSheetByName(SHEET_NAME);   
-  var changedCell= event.source.getActiveRange().getA1Notation();
-  var title = _encodeName(event.value);
   
   var url = 'https://isthereanydeal.com/ajax/game/info?plain=' + title;
   var response = UrlFetchApp.fetch(url);
@@ -83,27 +101,28 @@ function handleBundled(event){
   }
 }
 
+// Adds a timestamp when editing-----------------------------------------------------------------------
 function addDateStamp(event)
 { 
   var timezone = "GMT-5";
   var timestamp_format = "MM-dd-yyyy"; // Timestamp Format. 
   var updateColName = ON_UPDATE_HEADER;
   var timeStampColName = TIMESTAMP_HEADER;
-  var sheet = event.source.getSheetByName(SHEET_NAME); //Name of the sheet where you want to run this script.
 
   var actRng = event.source.getActiveRange();
   var editColumn = actRng.getColumn();
   var index = actRng.getRowIndex();
-  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues();
+  var headers = ss.getRange(1, 1, 1, s.getLastColumn()).getValues();
   var dateCol = headers[0].indexOf(timeStampColName);
   var updateCol = headers[0].indexOf(updateColName); updateCol = updateCol+1;
   if (dateCol > -1 && index > 1 && editColumn == updateCol) { // only timestamp if 'Last Updated' header exists, but not in the header row itself!
-    var cell = sheet.getRange(index, dateCol + 1);
+    var cell = ss.getRange(index, dateCol + 1);
     var date = Utilities.formatDate(new Date(), timezone, timestamp_format);
     cell.setValue(date);
   }
 }
 
+// Misc Methods------------------------------------------------------------------------------------------
 function _romanize (num) {
   var key = ["","i","ii","iii","iv","v","vi","vii","viii","ix"];
   return key[Number(num)];
